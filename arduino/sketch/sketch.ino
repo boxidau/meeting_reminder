f97b18c784d82f8ba1273145f991768bb8d7470d
Simon Mirco <smirco@fb.com>
2017-05-24 22:34:45 -0700

    Updated to show errors if no serial data for 30 seconds and added headlights and brake light support

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -2,6 +2,9 @@
 const int inputLED_a = 9;
 const int inputLED_b = 10;
 const int inputLED_c = 11;
+const int headlightLED = 14;
+const int rightBrakeLED = 15;
+const int leftBrakeLED = 16;
 
 String inputString = "";
 boolean eventReady = false;
@@ -10,7 +13,10 @@ int interval = 10;
 const int maxAlert = 5;
 
 unsigned short cyclePos = 0;
+unsigned long current = 0;
 unsigned long previous = 0;
+unsigned long lastSerialEvent = 0;
+unsigned long alertSerialInterval = 30000; // 30 sec
 
 //////////////////////
 // SETUP /////////////
@@ -20,6 +26,9 @@ void setup()
   Serial.begin(9600);  // Serial for debugging
   inputString.reserve(255);
   delay(1000); // allow us time to reprogram in case things are freaking out
+  pinMode(headlightLED, OUTPUT);
+  pinMode(rightBrakeLED, OUTPUT);
+  pinMode(leftBrakeLED, OUTPUT);
 }
 
 ////////////////////
@@ -28,23 +37,48 @@ void setup()
 void loop() 
 {
   serialEvent();
+  current = millis();
+  
   if (eventReady) {
+    lastSerialEvent = current;
     interval = inputString.toInt();
+    Serial.print(inputString);
     inputString = "";
     eventReady = false;
-    Serial.println(inputString);
   }
+
+  // alert by lighting LED's if no serial data 
+  // for alertSerialInterval milliseconds
+  if (current > lastSerialEvent + alertSerialInterval) {
+    illuminateLed(5);
+    illuminateLed(6);
+    illuminateLed(7);
+    illuminateLed(8);
+
+  // serial data present but out of range
+  } else if (interval > maxAlert || interval < 1) {
+    disableLeds();
   
-  unsigned long current = millis();
-  
-  if (current - previous >= interval * 50) {
+  // data in range, do flashing
+  } else if (current - previous >= interval * 50) { 
+    cycleLeds();
     previous = current;
-    cyclePos++;
-    if (interval > maxAlert) {
-      disableLeds();
-    } else {
-      illuminateLed(cyclePos % 6);
-    }
+  }
+
+}
+
+void cycleLeds() {
+  disableLeds();
+  cyclePos++;
+  illuminateLed(cyclePos % 6);
+  if (cyclePos % 3 == 0) {
+    illuminateLed(6);
+  }
+  if (cyclePos % 3 == 1) {
+    illuminateLed(7);
+  }
+  if (cyclePos % 3 == 2) {
+    illuminateLed(8);
   }
 }
 
@@ -66,10 +100,12 @@ void disableLeds() {
   pinMode(inputLED_a, INPUT);
   pinMode(inputLED_b, INPUT);
   pinMode(inputLED_c, INPUT);
+  digitalWrite(headlightLED, LOW);
+  digitalWrite(leftBrakeLED, LOW);
+  digitalWrite(rightBrakeLED, LOW);
 }
 
 void illuminateLed(int ledId) {
-
   if (ledId == 0) {
     // UP
     pinMode(inputLED_a, INPUT);
@@ -81,13 +117,13 @@ void illuminateLed(int ledId) {
   }
 
   if (ledId == 1) {
-    //RIGHT
-    pinMode(inputLED_a, INPUT);
+    // LEFT
+    pinMode(inputLED_a, OUTPUT);
     digitalWrite(inputLED_a, LOW);
-    pinMode(inputLED_b, OUTPUT);
-    digitalWrite(inputLED_b, LOW);
-    pinMode(inputLED_c, OUTPUT);
-    digitalWrite(inputLED_c, HIGH);
+    pinMode(inputLED_b, OUTPUT);    
+    digitalWrite(inputLED_b, HIGH);
+    pinMode(inputLED_c, INPUT);
+    digitalWrite(inputLED_c, LOW); 
   }
 
   if (ledId == 2) {
@@ -99,19 +135,29 @@ void illuminateLed(int ledId) {
     pinMode(inputLED_c, INPUT);
     digitalWrite(inputLED_c, LOW);
   }
-
+  
   if (ledId == 3) {
-    // LEFT
-    pinMode(inputLED_a, OUTPUT);
+    //RIGHT
+    pinMode(inputLED_a, INPUT);
     digitalWrite(inputLED_a, LOW);
-    pinMode(inputLED_b, OUTPUT);    
-    digitalWrite(inputLED_b, HIGH);
-    pinMode(inputLED_c, INPUT);
-    digitalWrite(inputLED_c, LOW); 
-  }    
+    pinMode(inputLED_b, OUTPUT);
+    digitalWrite(inputLED_b, LOW);
+    pinMode(inputLED_c, OUTPUT);
+    digitalWrite(inputLED_c, HIGH);
+  }
 
   if (ledId == 4) {
     // SPACE
+    pinMode(inputLED_a, OUTPUT);
+    digitalWrite(inputLED_a, LOW);
+    pinMode(inputLED_b, INPUT);
+    digitalWrite(inputLED_b, LOW);
+    pinMode(inputLED_c, OUTPUT);
+    digitalWrite(inputLED_c, HIGH);  
+  }
+  
+  if (ledId == 5) {
+    // CLICK
     pinMode(inputLED_a, OUTPUT);
     digitalWrite(inputLED_a, HIGH);
     pinMode(inputLED_b, INPUT);
@@ -120,14 +166,19 @@ void illuminateLed(int ledId) {
     digitalWrite(inputLED_c, LOW);
   }
 
-  if (ledId == 5) {
-    // CLICK
-    pinMode(inputLED_a, OUTPUT);
-    digitalWrite(inputLED_a, LOW);
-    pinMode(inputLED_b, INPUT);
-    digitalWrite(inputLED_b, LOW);
-    pinMode(inputLED_c, OUTPUT);
-    digitalWrite(inputLED_c, HIGH);  
+  if (ledId == 6) {
+    // HEADLIGHTS
+    digitalWrite(headlightLED, HIGH);
+  }
+
+  if (ledId == 7) {
+    // HEADLIGHTS
+    digitalWrite(leftBrakeLED, HIGH);
+  }
+
+  if (ledId == 8) {
+    // HEADLIGHTS
+    digitalWrite(rightBrakeLED, HIGH);
   }
 }
 
